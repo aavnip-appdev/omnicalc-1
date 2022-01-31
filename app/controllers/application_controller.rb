5c137fed3ed5561510b3d6d44b3f81788fcf1079
aavnip <aavnip@chicagobooth.edu>
2022-01-31 05:17:57 +0000

    Omnicalc WIP 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
 
-def blank_square_form
-  
-  render({ :template => "calculation_templates/square_form.html.erb"})
-end
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb"})
+  end
+
+  def calculate_square
+    #  Parameters: {"elephant"=>"42"}
+    @num = params.fetch("elephant").to_i
+    @square_of_num = @num ** 2 
+
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
 
 end
